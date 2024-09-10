a5c2dc98c58c01fa19414f518d5d2517fb686858
Siang Ling <siangling@Siangs-MacBook-Pro.local>
2024-09-09 21:24:06 -0500

    Updated App.js to use React Router v6 Routes component

--- src/App.js
+++ src/App.js
@@ -3,26 +3,18 @@ import MoviesList from "./home/MoviesList";
 import DetailedMoviesList from "./movies/DetailedMoviesList";
 import FullMovie from "./movie/FullMovie";
 import TheaterList from "./theaters/TheaterList";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Import Routes and Route
 
 function App() {
   return (
     <Router>
       <Header />
-      <Switch>
-        <Route exact path="/">
-          <MoviesList />
-        </Route>
-        <Route exact path="/movies">
-          <DetailedMoviesList />
-        </Route>
-        <Route exact path="/movies/:movieId">
-          <FullMovie />
-        </Route>
-        <Route exact path="/theaters">
-          <TheaterList />
-        </Route>
-      </Switch>
+      <Routes> {/* Replace Switch with Routes */}
+        <Route exact path="/" element={<MoviesList />} /> 
+        <Route exact path="/movies" element={<DetailedMoviesList />} /> 
+        <Route exact path="/movies/:movieId" element={<FullMovie />} /> 
+        <Route exact path="/theaters" element={<TheaterList />} /> 
+      </Routes>
     </Router>
   );
 }
